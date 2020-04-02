00704b89d5548abb8a00030d64419465d53cbf82
nosemasashi <ma.nose9413@gmail.com>
2020-04-02 13:48:30 +0900

    Itemドメインを作成しました。

--- src/main/java/com/example/demo/domain/Item.java
+++ src/main/java/com/example/demo/domain/Item.java
@@ -7,5 +7,91 @@ package com.example.demo.domain;
  *
  */
 public class Item {
+	/** ID */
+	private Integer id;
+	/** 商品名 */
+	private String name;
+	/** 状況 */
+	private Integer condition;
+	/** カテゴリー（ID） */
+	private Integer category;
+	/** ブランド名 */
+	private String brand;
+	/** 価格 */
+	private Integer price;
+	/** 配送 */
+	private Integer shipping;
+	/** 詳細 */
+	private String description;
+
+	@Override
+	public String toString() {
+		return "Item [id=" + id + ", name=" + name + ", condition=" + condition + ", category=" + category + ", brand="
+				+ brand + ", price=" + price + ", shipping=" + shipping + ", description=" + description + "]";
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getCondition() {
+		return condition;
+	}
+
+	public void setCondition(Integer condition) {
+		this.condition = condition;
+	}
+
+	public Integer getCategory() {
+		return category;
+	}
+
+	public void setCategory(Integer category) {
+		this.category = category;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	public Integer getShipping() {
+		return shipping;
+	}
+
+	public void setShipping(Integer shipping) {
+		this.shipping = shipping;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
 }
