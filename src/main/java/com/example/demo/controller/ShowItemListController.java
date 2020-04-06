22be1b8acbc6e96e21493913910d8ec650591488
nosemasashi <ma.nose9413@gmail.com>
2020-04-06 16:13:14 +0900

    ShowItemDetailControllerを作成しました。

--- src/main/java/com/example/demo/controller/ShowItemListController.java
+++ src/main/java/com/example/demo/controller/ShowItemListController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.domain.Item;
+import com.example.demo.service.ShowItemDetailService;
 import com.example.demo.service.ShowItemListService;
 
 /**
@@ -23,6 +24,9 @@ public class ShowItemListController {
 	@Autowired
 	private ShowItemListService showItemListService;
 
+	@Autowired
+	private ShowItemDetailService showItemDetailService;
+
 	/**
 	 * 商品一覧ページへ遷移します.
 	 * 
@@ -36,8 +40,19 @@ public class ShowItemListController {
 		return "item_list";
 
 	}
-	
-//	public String showItemDetail(Integer id) {
-//		
-//	}
+
+	/**
+	 * 
+	 * 商品詳細ページへ遷移します.
+	 * 
+	 * @param id 商品ID
+	 * @param model　リクエストスコープ作成
+	 * @return　商品詳細ページ
+	 */
+	public String showItemDetail(Integer id, Model model) {
+		Item item = showItemDetailService.showItemDetail(id);
+		model.addAttribute("item", item);
+		return "item_detail";
+
+	}
 }
