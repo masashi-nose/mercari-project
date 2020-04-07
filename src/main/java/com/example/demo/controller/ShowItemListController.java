b97ae6de9aa640ded8f771da1e33d14c04657c0f
nosemasashi <ma.nose9413@gmail.com>
2020-04-07 12:24:20 +0900

    ShowItemListControllerに商品曖昧検索結果を表示させるメソッドを追加しました。

--- src/main/java/com/example/demo/controller/ShowItemListController.java
+++ src/main/java/com/example/demo/controller/ShowItemListController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.domain.Item;
+import com.example.demo.form.SearchByItemNameForm;
 import com.example.demo.service.ShowItemListService;
 
 /**
@@ -37,4 +39,29 @@ public class ShowItemListController {
 
 	}
 
+	/**
+	 * 商品名で曖昧検索し、検索結果を表示します.
+	 * 
+	 * @param form   リクエストパラメータ
+	 * @param result
+	 * @param model  リクエストスコープ作成
+	 * @return 検索結果画面
+	 */
+	public String SearchByItemName(SearchByItemNameForm form, BindingResult result, Model model) {
+		List<Item> itemList = showItemListService.findByItemName(form.getName());
+
+		if (itemList == null) {
+			String itemName = form.getName();
+			result.rejectValue("name", "", "No results for");
+
+			model.addAttribute("itemName", itemName);
+			return showItemList(model);
+
+		}
+
+		model.addAttribute("itemList", itemList);
+
+		return "item_list";
+
+	}
 }
